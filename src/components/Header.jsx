3c2e738368ce71d3400480c55042c721826981a2
ga7axy <akshaymrajput@gmail.com>
2024-06-28 00:12:33 +0530

    fix ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     return (
         <div className="relative">
             <div className="w-full h-80 flex flex-col gap-10 justify-center items-center p-12 bg-slate-600 text-white">
-                <h1 className="text-4xl">Elevate Your Everyday</h1>
+                <h1 className="text-4xl text-center">Elevate Your Everyday</h1>
                 <div>
                     <Button
                         className="bg-amber-500 hover:bg-amber-700"
